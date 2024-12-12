f2ce8d1a1515834f1d2f823a549b5bf78ee3052d
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 18:33:35 +0900

    refactor(constant): 순위 순서 변경

--- src/main/java/com/example/lottore/constant/WinningPrize.java
+++ src/main/java/com/example/lottore/constant/WinningPrize.java
@@ -1,11 +1,11 @@
 package com.example.lottore.constant;
 
 public enum WinningPrize {
-    FIRST(6,false,2000000000),
-    SECOND(5,true,30000000),
-    THIRD(5,false,1500000),
+    FIFTH(3,false,5000),
     FOURTH(4,false,50000),
-    FIFTH(3,false,5000)
+    THIRD(5,false,1500000),
+    SECOND(5,true,30000000),
+    FIRST(6,false,2000000000),
     ;
     private int count;
     private boolean isBonusNumber;
