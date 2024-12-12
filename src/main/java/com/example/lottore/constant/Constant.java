350c76ce6187a27b364d1819792315e5f2edc4e6
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 16:55:23 +0900

    feat(constant): 로또 번호 갯수 상수 추가

--- src/main/java/com/example/lottore/constant/Constant.java
+++ src/main/java/com/example/lottore/constant/Constant.java
@@ -4,6 +4,7 @@ public enum Constant {
     LOTTO_COST(1000),
     LOTTO_NUMBER_RANGE_START(1),
     LOTTO_NUMBER_RANGE_END(45),
+    LOTTO_COUNT(6)
     ;
     private int value;
     private Constant(int value) {
