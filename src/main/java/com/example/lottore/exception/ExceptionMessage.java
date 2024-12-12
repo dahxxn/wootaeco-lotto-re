0c8ecc7ddafb75f27bc0ec90f4a869a84427623f
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 15:57:34 +0900

    feat(exception): 예외 메시지 추가

--- src/main/java/com/example/lottore/exception/ExceptionMessage.java
+++ src/main/java/com/example/lottore/exception/ExceptionMessage.java
@@ -1,6 +1,12 @@
 package com.example.lottore.exception;
 
 public enum ExceptionMessage {
+    NOT_NUMBER("숫자가 아닌 값이 올 수 없습니다. "),
+    WRONG_PAY_COST_LACK_MONEY("구입 금액은 로또 가격보다 커야합니다. "),
+    WRONG_PAY_COST("구입 금액은 로또 가격으로 나누어 떨어져야 합니다. "),
+    WRONG_WINNING_NUMBERS_COUNT("당첨 번호는 6개가 입력되어야 합니다. "),
+    WRONG_LOTTO_NUMBER_RANGE("로또 번호 범위를 벗어납니다. "),
+    WRONG_LOTTO_NUMBER_DUPLICATED("중복된 번호가 존재하면 안됩니다.  ")
     ;
 
     private final String ERROR_MESSAGE_HEAD = "[ERROR] ";
