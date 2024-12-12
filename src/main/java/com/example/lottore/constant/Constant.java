ff0bc0eaab5749aa8b79ef686c211c30f13ffee8
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 15:58:21 +0900

    feat(constant): 로또에서 사용할 상수 저장하는 enum 클래스 생성

---
+++ src/main/java/com/example/lottore/constant/Constant.java
@@ -0,0 +1,15 @@
+package com.example.lottore.constant;
+
+public enum Constant {
+    LOTTO_COST(1000),
+    LOTTO_NUMBER_RANGE_START(1),
+    LOTTO_NUMBER_RANGE_END(45),
+    ;
+    private int value;
+    private Constant(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
