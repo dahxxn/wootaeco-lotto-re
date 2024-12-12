9e678bcca0043d1ac2a213dbd4c6d4bc70f4dc9a
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 18:33:11 +0900

    fix(constant): 프린트 포멧 문자열 일부 수정

--- src/main/java/com/example/lottore/constant/PrintMessage.java
+++ src/main/java/com/example/lottore/constant/PrintMessage.java
@@ -6,9 +6,10 @@ public enum PrintMessage {
     INPUT_WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요. "),
     INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요. "),
     PRINT_WINNING_STATISTICS_MESSAGE("당첨 통계"),
+    PRINT_HORIZONTAL_LINE("---"),
     PRINT_WINNING_STATE_MESSAGE("%d개 일치 (%,d원) - %d개"),
-    PRINT_BONUS_WINNING_STATE_MESSAGE("%d개 일치, 보너스 볼 일치 (%d,원) - %d개"),
-    PRINT_TOTAL_RATE_OF_RETURN_MESSAGE("총 수익률은 %.2f% 입니다.")
+    PRINT_BONUS_WINNING_STATE_MESSAGE("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
+    PRINT_TOTAL_RATE_OF_RETURN_MESSAGE("총 수익률은 %.1f%%입니다.")
     ;
     private final String message;
 
