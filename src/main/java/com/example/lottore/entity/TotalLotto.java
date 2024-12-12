8b58d4806e937fdfc97c4b859548391a418256f8
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 17:13:11 +0900

    feat(totalLotto): 모든 로또에 대한 당첨 유무 확인 로직 추가

--- src/main/java/com/example/lottore/entity/TotalLotto.java
+++ src/main/java/com/example/lottore/entity/TotalLotto.java
@@ -2,14 +2,22 @@ package com.example.lottore.entity;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import com.example.lottore.constant.Constant;
+import com.example.lottore.constant.WinningPrize;
+import com.example.lottore.dto.WinningResult;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class TotalLotto {
     List<Lotto> totalLotto;
+    HashMap<WinningPrize, Integer> winningResult;
 
     public TotalLotto() {
         totalLotto = new ArrayList<Lotto>();
+        winningResult = new HashMap<>();
+        for(WinningPrize prize : WinningPrize.values()) {
+            winningResult.put(prize,0);
+        }
     }
 
     public void buyLottos(int lottoCount){
@@ -25,6 +33,20 @@ public class TotalLotto {
 
     private List<Integer> generateLottoNumbers(){
         return Randoms.pickUniqueNumbersInRange(Constant.LOTTO_NUMBER_RANGE_START.getValue(), Constant.LOTTO_NUMBER_RANGE_END.getValue(), Constant.LOTTO_COUNT.getValue());
+    }
+
+    public void checkTotalWinningResult(List<Integer> winningNumbers, int bonusNumber){
+        for(Lotto lotto : totalLotto){
+            WinningResult result = lotto.checkWinningNumbers(winningNumbers,bonusNumber);
+            WinningPrize prize = lotto.findPrize(result.getWinningNumberCount(), result.getIsBonusNumber());
+            addWinningResult(prize);
+        }
+    }
+
+    private void addWinningResult(WinningPrize prize){
+        if(prize != null){
+            winningResult.put(prize,winningResult.get(prize)+1);
+        }
     }
 
 }
